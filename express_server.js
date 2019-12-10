e9f974fd673952e1ef840f5e6f73ab7416b10467
Anisa <anisahamohamed@gmail.com>
2019-12-10 18:17:55 +0000

    added delete buuton and redirect fixed after shorturl generated

--- express_server.js
+++ express_server.js
@@ -6,7 +6,18 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+function generateRandomString(longURL) {
+  
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,16 +31,38 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  let randomUrl = generateRandomString(req.body.longURL);
+  urlDatabase[randomUrl] = req.body.longURL;
+  console.log(urlDatabase)
+  //res.send(randomUrl);   
+  res.redirect('/urls')      // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase, greeting: "Hi"};
   res.render("urls_index", templateVars);
  });
- 
+
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
  app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
- 
+app.get("/u/:shortURL", (req, res) => {
+   const longURL =  urlDatabase[req.params.shortURL]
+   console.log(longURL)
+  res.redirect(longURL);
+});
+app.post("/urls/:shortURL/delete",(req,res) =>{
+  delete urlDatabase[req.params.shortURL];
+  console.log(urlDatabase)
+  res.redirect('/urls');
+
+})
 //  app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 //  });
